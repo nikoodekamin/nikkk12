3ad7faf78dac9eb18945bcdd624528d89bccfee2
N Deca <133411537+nikoodekamin@users.noreply.github.com>
2023-05-18 15:09:11 +0330

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,3 +5,11 @@ print(c)
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
+nums = (1, 2, 3, 4)
+
+sum_nums = 0
+
+for num in nums:
+    sum_nums = sum_nums + num
+
+print(f'Sum of numbers is {sum_nums}')
